fdd47f12c1ea364926cc016687a0ae28c1963be0
Swift Funcional <info@swiftfuncional.com>
2017-03-16 23:38:51 +0100

    Using protocols

--- Continuation passing style.playground/Contents.swift
+++ Continuation passing style.playground/Contents.swift
@@ -5,6 +5,14 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 class Model {
 
+
+}
+
+protocol AsynchronousCountable {
+	func count(f: @escaping (Int) -> ())
+}
+
+extension Model: AsynchronousCountable {
 	func count(f: @escaping (Int) -> Void) {
 		DispatchQueue.main.async {
 			f(0)
@@ -12,19 +20,25 @@ class Model {
 	}
 }
 
-func whenEmpty(model: Model, _ onTrue: @escaping (Void) -> Void, otherwise onFalse: @escaping (Void) -> Void) {
-	model.count { (count) in
-		if count == 0 {
-			onTrue()
-		} else {
-			onFalse()
+protocol CanBeEmpty : AsynchronousCountable {
+	func whenEmpty(_ onTrue: @escaping () -> (), otherwise onFalse: @escaping () -> ())
+}
+
+extension Model: CanBeEmpty {
+	func whenEmpty(_ onTrue: @escaping (Void) -> Void, otherwise onFalse: @escaping (Void) -> Void) {
+		self.count { (count) in
+			if count == 0 {
+				onTrue()
+			} else {
+				onFalse()
+			}
 		}
 	}
 }
 
 let model = Model()
 
-whenEmpty(model: model, {
+model.whenEmpty({
 	//no registers
 }, otherwise: {
 	//there are some registers
